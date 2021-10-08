e71661d2aae74752adf648da4a40d3a3c889e7c2
Filip Petkovski <flp_petkovski@yahoo.com>
2021-10-08 23:02:07 +0200

    fix  linkedIn address

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -21,7 +21,7 @@ const Sidebar = () => {
             <div>
              <div className="flex justify-around w-9/12 mx-auto my-5 text-green md:w-full">
             <a href="https://github.com/flppetkovski?tab=re positories"><AiFillGithub className="w-8 h-8 cursor-pointer" aria-label="github profile"/></a>
-            <a href="https://www.linkedin.com/in/https://www.linkedin.com/in/filip-petkovski-06766b100/" aria-label="linkedIn profile" ><AiFillLinkedin className="w-8 h-8 cursor-pointer"/></a>
+            <a href="https://www.linkedin.com/in/filip-petkovski-06766b100/" aria-label="linkedIn profile" ><AiFillLinkedin className="w-8 h-8 cursor-pointer"/></a>
             </div> 
                 <div className="py-4 my-5 bg-gray-200 dark:bg-dark-200" style={{marginLeft: "-1rem", marginRight: "-1rem"}}>
                     <div className="flex items-center justify-center space-x-2"><GoLocation />
