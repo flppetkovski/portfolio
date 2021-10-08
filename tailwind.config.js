4413fa8c2b64f1cd8b64ab2ae245f2d2226f87ab
Filip Petkovski <flp_petkovski@yahoo.com>
2021-10-08 22:09:44 +0200

    problems with styles with tailwind deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.tsx', './components/**/*.tsx}'],
+  purge: ["./pages/**/*.ts", "./components/**/*.tsx"],
     darkMode: "class", // or 'media' or 'class'
   theme: {
     fontFamily: {
