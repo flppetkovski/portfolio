2680d91cc5c531b47c66f79ad492ab316312f357
Filip Petkovski <flp_petkovski@yahoo.com>
2021-10-08 21:48:05 +0200

    linting issue fixed

--- pages/api/services.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-import { services } from '../../data';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-
-  res.status(200).json({services})
-}
