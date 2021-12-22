527c474781759759ede4954f0246e544ce097522
Filip Petkovski <flp_petkovski@yahoo.com>
2021-12-22 09:06:31 +0100

    typo fix

--- data.ts
+++ data.ts
@@ -131,7 +131,7 @@ key_techs:["next.js", "tailwind", "markdown"]
             name: "Weather PWA",
             description: "PWA for getting weather.",
             image_path: "./public/images/weather-pwa.PNG",
-            deployed_url: "https://petkovski-expense-calculator.netlify.app/",
+            deployed_url: "https://petkovski-weather.netlify.app/",
             github_url: "https://github.com/flppetkovski/weather-pwa",
             category: ["react"], 
             key_techs:["react", "progressive web app - PWA" ]
@@ -140,7 +140,7 @@ key_techs:["next.js", "tailwind", "markdown"]
             name: "Expense calculator",
             description: "This is the first project I ever built at the end of 2018, displayed for reference only. It's for For Desktop only.",
             image_path: "/images/expense-calculator.PNG",
-            deployed_url: "https://petkovski-weather.netlify.app/",
+            deployed_url: "https://petkovski-expense-calculator.netlify.app/",
             github_url: "https://github.com/flppetkovski/expense-calculator-fe",
             category: ["react", "express", "node", "mongo"], 
             key_techs:["react", "materialUI", "mongoDB", "node", "express" ]
